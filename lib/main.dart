5fdcd931a793a64a45cc8db89894d32f91a84082
ingineerOrjun <engineering4genius@gmail.com>
2022-07-18 23:58:56 +0545

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "profile app",
+    home: Scaffold(
+      // ignore: prefer_const_constructors
+      appBar: AppBar(title: Text('My profile app')),
+    ),
+  ));
+}
